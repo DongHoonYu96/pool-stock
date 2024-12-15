0aa67fcda0a581f8b4cbe793467ef616fc944fa0
DongHoonYu96 <sbna154@gmail.com>
2024-12-15 19:47:55 +0900

    init commit

---
+++ src/main/java/com/stock/pool_stock/PoolStockApplication.java
@@ -0,0 +1,13 @@
+package com.stock.pool_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoolStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoolStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stock/pool_stock/PoolStockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stock.pool_stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoolStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
